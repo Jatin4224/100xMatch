67b3ecadd720e19fc2d78648b478d6e013d017f0
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-12-14 02:35:31 +0530

    step 4 - adding app.use(express.json()); middleware

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,9 +3,10 @@ const app = express();
 require("dotenv").config();
 const connectDb = require("./config/db");
 const port = process.env.PORT || 4000;
-
+app.use(express.json());
 connectDb();
 
+app.post("/signup", (req, res) => {});
 app.listen(port, () => {
   console.log("server running fine");
 });
