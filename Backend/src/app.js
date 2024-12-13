5a487fb5526a979364ae876b923367a04e595c44
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-12-14 02:53:41 +0530

    step 5-> hashing password

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,11 +2,25 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const connectDb = require("./config/db");
+const bcrypt = require("bcrypt");
 const port = process.env.PORT || 4000;
 app.use(express.json());
+const User = require("./models/user");
 connectDb();
 
-app.post("/signup", (req, res) => {});
+app.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+
+  //hashing
+  const hashedPassowrd = await bcrypt.hash(password, 10);
+  const user = new User({
+    email,
+    password: hashedPassowrd,
+  });
+
+  await user.save();
+  res.send("success");
+});
 app.listen(port, () => {
   console.log("server running fine");
 });
