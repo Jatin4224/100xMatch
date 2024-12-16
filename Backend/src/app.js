bdcfc6f13ea62521e0270d4ee1a519ad8c1bf826
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-12-16 05:42:23 +0530

    step 11 - routing

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,77 +2,17 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const connectDb = require("./config/db");
-const bcrypt = require("bcrypt");
 const port = process.env.PORT || 4000;
-const jwtsecret = process.env.JWT_SECRET;
-const jwt = require("jsonwebtoken");
-const User = require("./models/user");
 const cookieParser = require("cookie-parser");
-const validatedData = require("./utils/validate");
-const userAuth = require("./middleware/auth");
+const authRouter = require("../src/routes/auth");
+const profileRouter = require("../src/routes/profile");
 app.use(express.json());
 app.use(cookieParser());
 connectDb();
 
-app.post("/signup", async (req, res) => {
-  try {
-    validatedData(req);
-    const { email, password, firstName, lastName } = req.body;
+app.use("/api/v1", authRouter);
+app.use("/api/v1", profileRouter);
 
-    //hashing
-    const hashedPassowrd = await bcrypt.hash(password, 10);
-    const user = new User({
-      firstName,
-      lastName,
-      email,
-      password: hashedPassowrd,
-    });
-
-    await user.save();
-    res.send("success");
-  } catch (error) {
-    res.status(404).json({
-      message: error.message,
-    });
-  }
-});
-
-app.post("/signin", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email: email });
-    if (!user) {
-      throw new Error("user is not found");
-    }
-
-    const loggedInUser = await bcrypt.compare(password, user.password);
-    if (loggedInUser) {
-      const token = jwt.sign({ _id: user._id }, jwtsecret);
-      res.cookie("token", token);
-
-      //verify the token
-      res.status(200).json({
-        message: `logged in user ${user}`,
-      });
-    } else {
-      res.status(404).send("user not found");
-    }
-  } catch (err) {
-    res.send(err.message);
-  }
-});
-
-app.get("/profile", userAuth, async (req, res) => {
-  try {
-    const user = req.user;
-    res.send(user);
-  } catch (error) {
-    res.status(400).json({
-      message: "error" + error.message,
-    });
-  }
-});
 app.listen(port, () => {
   console.log("server running fine");
 });
---
+++ Backend/src/routes/auth.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const authRouter = express.Router();
+const validatedData = require("../utils/validate");
+const jwtsecret = process.env.JWT_SECRET;
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+
+authRouter.post("/signup", async (req, res) => {
+  try {
+    validatedData(req);
+    const { email, password, firstName, lastName } = req.body;
+
+    //hashing
+    const hashedPassowrd = await bcrypt.hash(password, 10);
+    const user = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassowrd,
+    });
+
+    await user.save();
+    res.send("success");
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+});
+
+authRouter.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      throw new Error("user is not found");
+    }
+
+    const loggedInUser = await bcrypt.compare(password, user.password);
+    if (loggedInUser) {
+      const token = jwt.sign({ _id: user._id }, jwtsecret, {
+        expiresIn: "24h",
+      });
+      res.cookie("token", token);
+
+      //verify the token
+      res.status(200).json({
+        message: `logged in user ${user}`,
+      });
+    } else {
+      res.status(404).send("user not found");
+    }
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
+module.exports = authRouter;
