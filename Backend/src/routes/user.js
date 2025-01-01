7ddcaad3d88a44303212a59cade3b34e3360adf7
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-01-01 18:44:02 +0530

    .

--- Backend/src/routes/user.js
+++ Backend/src/routes/user.js
@@ -3,19 +3,39 @@ const Request = require("../models/request");
 const userRouter = express.Router();
 const userAuth = require("../middleware/auth");
 
+const USER_SAFE_DATA = "firstName LastName age gender about";
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
     const connectionRequest = await Request.findOne({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate("fromUserId", "firstName LastName age gender about");
+    }).populate("fromUserId", USER_SAFE_DATA);
 
     res.send(connectionRequest);
   } catch (err) {
     res.send(err.message);
   }
 });
-//get/user/requests
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequest = await Request.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    }).populate("fromUserId", USER_SAFE_DATA);
+
+    const data = connectionRequest.map((row) => row.fromUserId);
+
+    res.json({ data });
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+});
+
 //user/feed
 module.exports = userRouter;
