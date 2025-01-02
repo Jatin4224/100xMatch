eed76e5fe18d984858e74156cac46ac4f7d31ee4
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-01-02 19:48:33 +0530

    feed api complete

--- Backend/src/routes/user.js
+++ Backend/src/routes/user.js
@@ -48,6 +48,9 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 userRouter.get("/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
+    const page = ParseInt(req.params.page) || 1;
+    const limit = ParseInt(req.params.limit) || 10;
+    const skip = (page - 1) * limit;
     //find all connection requests (sent + received)
     const connectionRequests = await Request.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
@@ -66,7 +69,10 @@ userRouter.get("/feed", userAuth, async (req, res) => {
           _id: { $ne: loggedInUser._id },
         },
       ],
-    }).select(USER_SAFE_DATA);
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
 
     res.send(users);
   } catch (err) {
