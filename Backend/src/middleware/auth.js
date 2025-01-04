bac4022bf00f4962575c90b3372c14f4cc87e6e0
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-01-05 02:47:10 +0530

    .

--- Backend/src/middleware/auth.js
+++ Backend/src/middleware/auth.js
@@ -6,9 +6,11 @@ const userAuth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
     if (!token) {
-      throw new Error("unauthorized");
+      return res.status(401).json({
+        message: "Not logged In",
+      });
     }
-
+    s;
     const decodedMessage = await jwt.verify(token, process.env.JWT_SECRET);
 
     const { _id } = decodedMessage;
--- frontend/frontend/src/components/Body.jsx
+++ frontend/frontend/src/components/Body.jsx
@@ -1,7 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 const Body = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view");
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <Navbar />
